707f93f57b00255770efbc9d4744c595f178f1bb
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-04-21 15:40:00 +0200

    adding the footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
+import Advert from "@/components/Advert";
+import CarouselFinal from "@/components/CarouselFinal";
+import Filter from "@/components/Filter";
 import Navbar from "@/components/Navbar/Navbar";
+import Solutions from "@/components/Solutions/Solutions";
+import Footer from "@/components/Footer";
 import Hero from "@/components/hero/Hero";
+import SecondHero from "@/components/hero/SecondHero";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
       <Hero></Hero>
-      <div className="h-[680px]">
+      <section className="h-[680px]">
       <Solutions></Solutions>
+      </section>
+      <section className="h-[720px] bg-filter-image bg-cover pt-[220px]">
+      <Filter></Filter>
+      </section>
+      <section className="mt-20">
+      <div className="h-[1025px] bg-filter-second-image bg-no-repeat bg-[length:100%_496px]">
+      <SecondHero></SecondHero>
       </div>
+      <div className="mx-auto">
+        <Advert></Advert>
+      </div>
+      <CarouselFinal></CarouselFinal>
+      </section>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </main>
     
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroImg from "@/assets/images/footer-image.png"
+const Footer = () => {
+  return (
+    <div>
+    <img className='w-[100%] ' src={heroImg.src} alt="" />
+    
+    </div>
+  )
+}
+
+export default Footer
