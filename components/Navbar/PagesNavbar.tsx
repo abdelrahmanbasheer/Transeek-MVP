c376818321b682d25e7de31e7c8cd945b8b61264
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-06-15 01:40:37 +0300

    adjusted navbbar to dynamic

--- components/Navbar/PagesNavbar.tsx
+++ components/Navbar/PagesNavbar.tsx
@@ -6,7 +6,7 @@ const PagesNavbar = ({title }:Props) => {
   return (
     <div className='h-[500px] bg-warehouse-page-image bg-cover'>
       <LowerNavbar></LowerNavbar>
-      <h1 className='text-6xl text-white capitalize p-36'>{title}</h1>
+      <h1 className='text-6xl text-white font-bold capitalize p-36'>{title}</h1>
       </div>
   )
 }
