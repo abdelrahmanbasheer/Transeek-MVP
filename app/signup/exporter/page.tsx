30baf5376dd6199651b03729d5303f608324294f
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-06-30 15:34:08 +0300

    created login and signup

---
+++ app/signup/exporter/page.tsx
@@ -0,0 +1,86 @@
+'use client'
+import React, { useState } from 'react'
+import logo from "@/assets/icons/logoXL.png";
+import { useForm } from 'react-hook-form';
+import axios from "axios"
+const page = () => {
+    interface IFormInput {
+      email: string;
+        password: string;
+        username: string;
+        country: string;
+        companyName: string;
+        phoneNumber: string;
+        typeOfGoods:string;
+        
+      }
+    const { register, handleSubmit } = useForm<IFormInput>({
+      defaultValues: {
+        email:"",
+        password:"",
+        username:"",
+        country:"",
+        companyName:"",
+        phoneNumber:"",
+        typeOfGoods:"",
+      },
+    });
+    
+      const [curr, setCurr] = useState("freightforwarder")
+      const [response, setResponse] = useState(null);
+      console.log(curr)
+      const onSubmit= async (formData:IFormInput)=>{
+        
+        try {
+          const response = await axios.post('http://localhost:3000/auth/signup/exporter', formData, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          setResponse(response.data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      } 
+       return (
+    <div className='flex'>
+        <img src="../../../public/login-page-bg.png" className='h-screen' alt="" />
+        <div className='flex flex-col mt-10 w-[500px]  mx-auto'>
+            <img src={logo.src} className='w-[609px] ' alt="" />
+            <h1 className='text-[#313131] text-4xl font-bold mt-6 '>Sign up</h1>
+            <h1 className='text-[#313131] text-sm my-3 '>Let's get you all set up so you can access your personal account.</h1>
+            <form className='flex flex-col gap-6' onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex gap-6 relative'>
+            <label className='text-[#313131] z-50 left-5 top-[-10px] bg-white text-sm absolute'>User Name</label>
+            <input  className='p-4 border-2  border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("username")} />
+            <label className='text-[#313131] text-sm z-50 left-[300px] top-[-10px] absolute bg-white'>Company Name</label>
+            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("companyName")} />
+            </div>
+            <div className='flex gap-6 relative'>
+            <label className='text-[#313131] z-50 left-5 top-[-10px] bg-white text-sm absolute'>Country</label>
+            <input  className='p-4 border-2  border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("country")} />
+            <label className='text-[#313131] text-sm z-50 left-[300px] top-[-10px] absolute bg-white'>Phone Number</label>
+            <input type='number' className='p-4 border-2 border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("phoneNumber")} />
+            </div>
+            <div className='flex flex-col gap-6 relative'>
+            <label className='text-[#313131] text-sm  z-50 left-5 top-[-10px] absolute bg-white'>Email</label>
+            <input type='email'  className='p-4 border-2 border-[#79747E] rounded-md w-[537px] h-[56px] outline-none' {...register("email")} />
+            <label className='text-[#313131] text-sm  z-50 left-5 top-[70px] absolute bg-white'> password</label>
+            <input type='password' className='p-4 border-2 border-[#79747E] rounded-md w-[537px] h-[56px] outline-none' {...register("password")} />
+            <label className='text-[#313131] text-sm  z-50 left-5 top-[150px] absolute bg-white'> type Of Goods </label>
+            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[537px] h-[56px] outline-none' {...register("typeOfGoods")} />
+            </div>
+            <div className=' text-sm font-semibold text-[#312e33] flex flex-col ml-2'>
+        
+                <p className='mt-3'>By signing up, you agree to our <span className='text-[#353d92] font-bold'><a href="/terms">Terms of Service</a></span> and <span className='text-[#353d92] font-bold'><a href="/privacy">Privacy Policy</a></span></p>
+            </div>
+            <button className='w-[512px] bg-[#515DEF] rounded-md p-4 text-white mt-2' type="submit">Sign up</button>
+            <p className='text-center mt-3 text-sm font-semibold'>Already have an account?<span className='text-[#353d92] font-bold'><a href="/login"> Login </a></span></p>
+            </form>
+            {response && <p className='text-center mt-4'>{JSON.stringify(response)}</p>}
+        </div>
+    </div>
+  )
+}
+
+export default page
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,52 +1,22 @@
-'use client'
+
 import React from 'react'
 import logo from "@/assets/icons/logoXL.png";
-import { useForm } from 'react-hook-form';
+import Link from 'next/link';
 const page = () => {
-    interface IFormInput {
-        email: string;
-        password: string;
-      }
-    const { register, handleSubmit } = useForm();
-
-  return (
+       return (
     <div className='flex'>
-        <img src="./login-page-bg.png" className='h-screen' alt="" />
+        <img src=".././login-page-bg.png" className='h-screen' alt="" />
         <div className='flex flex-col mt-10 w-[500px]  mx-auto'>
             <img src={logo.src} className='w-[609px] ' alt="" />
-            <h1 className='text-[#313131] text-4xl font-bold mt-6 '>Sign up</h1>
+            <h1 className='text-[#313131] text-4xl font-bold mt-6'>Sign up</h1>
             <h1 className='text-[#313131] text-sm my-3 '>Let's get you all set up so you can access your personal account.</h1>
-            <form className='flex flex-col gap-6'>
-            <div className='flex gap-6 relative'>
-            <label className='text-[#313131] z-50 left-5 top-[-10px] bg-white text-sm absolute'>First Name</label>
-            <input  className='p-4 border-2  border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("email")} />
-            <label className='text-[#313131] text-sm z-50 left-[300px] top-[-10px] absolute bg-white'>Last Name</label>
-            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("email")} />
-            </div>
-            <div className='flex gap-6 relative'>
-            <label className='text-[#313131] z-50 left-5 top-[-10px] bg-white text-sm absolute'>Email</label>
-            <input  className='p-4 border-2  border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("email")} />
-            <label className='text-[#313131] text-sm z-50 left-[300px] top-[-10px] absolute bg-white'>Phone Number</label>
-            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[312px] h-[56px] outline-none' {...register("email")} />
-            </div>
-            <div className='flex flex-col gap-6 relative'>
-            <label className='text-[#313131] text-sm  z-50 left-5 top-[-10px] absolute bg-white'>Password</label>
-            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[537px] h-[56px] outline-none' {...register("password")} />
-            <label className='text-[#313131] text-sm  z-50 left-5 top-[70px] absolute bg-white'>Confirm password</label>
-            <input  className='p-4 border-2 border-[#79747E] rounded-md w-[537px] h-[56px] outline-none' {...register("password")} />
-            </div>
-            <div className=' text-sm font-semibold text-[#312e33] flex flex-col ml-2'>
-              <form className=''>
-            <input className='' type="radio" id="option1" name="option" value="1"/>
-            <label className='ml-3 mb-3'>Exporter</label><br />
-            <input  type="radio" id="option2" name="option" value="2"/>
-            <label className='ml-3 mt-3'>Freight forwarder</label>
-                </form>
-                <p className='mt-3'>By signing up, you agree to our <span className='text-[#353d92] font-bold'><a href="/terms">Terms of Service</a></span> and <span className='text-[#353d92] font-bold'><a href="/privacy">Privacy Policy</a></span></p>
-            </div>
-            <button className='w-[512px] bg-[#515DEF] rounded-md p-4 text-white mt-2' type="submit">Sign up</button>
+            <Link href={"/signup/exporter"}>
+            <button className='w-[512px] bg-[#515DEF] rounded-md p-4 text-white mt-2' type="submit">Exporter</button>
+            </Link>
+            <Link href={"/signup/freightforwarder"}>
+            <button className='w-[512px] bg-[#515DEF] rounded-md p-4 text-white mt-2' type="submit">Freight-Forwarder</button>
+            </Link>
             <p className='text-center mt-3 text-sm font-semibold'>Already have an account?<span className='text-[#353d92] font-bold'><a href="/login"> Login </a></span></p>
-            </form>
         </div>
     </div>
   )
